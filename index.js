74b84ecb4362e09c04b253482fe22d1898bd2766
Giuseppe Aquino <aqg@ti8m.ch>
2018-01-18 17:02:28 +0100

    Implemented Genre Route + Genre Model. Refactoring Routes to another file.

--- index.js
+++ index.js
@@ -14,81 +14,10 @@ const port = process.env.PORT || 8080;
 const mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost:27017/movies');
 
-const Foo = require('./app/models/foo');
-
-// ROUTES FOR THE API
-// =============================================================================
-const router = express.Router();
-
-router.use(function (req, res, next) {
-    console.log('Request has been received...something\'s happening');
-    next();
-});
-
-router.get('/test', function (req, res) {
-    res.json({message: 'Hooray! The API is answering...'})
-});
-
-router.route('/foo')
-    .post(function (req, res) {
-        let foo = new Foo();
-        foo.name = req.body.name;
-
-        foo.save(function (err) {
-            if (err) {
-                res.send(err);
-            }
-            res.json({message: 'Foo created!'});
-        });
-    })
-    .get(function (req, res) {
-        Foo.find(function (err, foos) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(foos);
-        })
-    });
-
-router.route('/foo/:foo_id')
-    .get(function (req, res) {
-        Foo.findById(req.params.foo_id, function (err, foo) {
-            if (err) {
-                res.send(err);
-            }
-            res.json(foo);
-        });
-    })
-    .put(function (req, res) {
-        Foo.findById(req.params.foo_id, function (err, foo) {
-            if (err) {
-                res.send(err);
-            }
-            foo.name = req.body.name;
-
-            foo.save(function (err) {
-                if (err) {
-                    res.send(err);
-                }
-                res.json({message: 'Foo Updated'});
-            })
-        })
-    })
-    .delete(function (req, res) {
-        Foo.remove({
-            _id: req.params.foo_id
-        }, function (err, foo) {
-            if (err) {
-                res.send(err);
-            }
-            res.json({message: 'Foo Deleted Successfully!'});
-        })
-    });
-
 // REGISTER ROUTES -------------------------------
 // all of the routes will be prefixed with /api
 // =============================================================================
-app.use('/api', router);
+app.use('/api', require('./app/routes/router'));
 
 // START THE SERVER
 // =============================================================================
